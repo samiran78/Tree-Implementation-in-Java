9b03d2ca4dd777af12f2d09a6ec79a41a272ad0f
Samiran Royy <sroy21191@gmail.com>
2026-01-14 01:45:21 +0530

    no message

---
+++ BinaryTree.java
@@ -0,0 +1,13 @@
+class  Node{
+    int data;
+    Node left;
+    Node right;
+    public Node(){
+        this.data = data;
+    }
+}
+public class BinaryTree {
+
+    public void insert(int i) {
+    }
+}
---
+++ TreeImplementation.java
@@ -0,0 +1,7 @@
+public class TreeImplementation {
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        tree.insert(8);
+
+    }
+}
